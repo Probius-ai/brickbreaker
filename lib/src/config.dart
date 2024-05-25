d53bc7763cd914442717352f21009564df79a860
Probius-ai <kcfm0529@naver.com>
2024-05-25 23:28:45 +0900

    Added brick color to config.dart

--- lib/src/config.dart
+++ lib/src/config.dart
@@ -1,7 +1,23 @@
+import 'package:flutter/material.dart';
+
+const brickColors = [
+  Colors.red,
+  Colors.orange,
+  Colors.yellow,
+  Colors.green,
+  Colors.blue,
+  Colors.purple,
+];
+
 const gameWidth = 800.0;
 const gameHeight = 1600.0;
 const ballRadius = gameWidth *0.02;
 const batWidth = gameWidth * 0.2;
 const batHeight = gameHeight * 2;
 const batStep = gameWidth * 0.05;
+const brickGutter = gameWidth * 0.015;
+final brickWidth = 
+  (gameWidth - (brickGutter * (brickColors.length + 1))) / brickColors.length;
+
+final brickHeight = gameHeight * 0.03;
 const difficultyModifier = 1.0;
