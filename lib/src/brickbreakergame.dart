f318ec5ba00488d6a91f39adaaf56f787524e1ed
Probius-ai <kcfm0529@naver.com>
2024-05-25 22:18:35 +0900

    Finish

--- lib/src/brickbreakergame.dart
+++ lib/src/brickbreakergame.dart
@@ -7,7 +7,7 @@ import 'objects/obj.dart';
 import 'config.dart';
 
 
-class BrickBreaker extends FlameGame{
+class BrickBreaker extends FlameGame with HasCollisionDetection {
   // Add your class members and methods here
   BrickBreaker()
     : super(
@@ -28,14 +28,14 @@ class BrickBreaker extends FlameGame{
     camera.viewfinder.anchor = Anchor.topLeft;
 
     world.add(GameArea());
-    
+
     // Add the ball to the game
     world.add(Ball(
       radius: ballRadius,
       position: size / 2,
       velocity: Vector2((rand.nextDouble() -0.5)*width,height * 0.2) // Random velocity
-    .normalized()
-    ..scale(height/4)));
+        .normalized()
+        ..scale(height/4)));
 
     debugMode = true;
   }
