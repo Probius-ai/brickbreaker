7fe8eef503e8ada986197ef559b68d7143ac0370
Probius-ai <kcfm0529@naver.com>
2024-05-26 20:50:51 +0900

    file for UI with playstate print on screen

--- lib/src/UI/game_app.dart
+++ lib/src/UI/game_app.dart
@@ -1,41 +1,75 @@
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class GameApp extends StatefulWidget {
-  @override
-  _GameAppState createState() => _GameAppState();
-}
+import '../brickbreakergame.dart';
+import '../config.dart';
 
-class _GameAppState extends State<GameApp> {
-  bool _isGameWon = false;
-  bool _isGameLost = false;
+class GameApp extends StatelessWidget {
+  const GameApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Brick Breaker'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        textTheme: GoogleFonts.pressStart2pTextTheme().apply(
+          bodyColor: const Color(0xff184e77),
+          displayColor: const Color(0xff184e77),
+        ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (_isGameWon)
-              Text(
-                'Congratulations! You won the game!',
-                style: TextStyle(fontSize: 24),
-              ),
-            if (_isGameLost)
-              Text(
-                'Game Over! You lost the game.',
-                style: TextStyle(fontSize: 24),
-              ),
-            ElevatedButton(
-              onPressed: () {
-                // TODO: Add game logic here
-              },
-              child: Text('Start Game'),
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color(0xffa9d6e5),
+                Color(0xfff2e8cf),
+              ],
             ),
-          ],
+          ),
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Center(
+                child: FittedBox(
+                  child: SizedBox(
+                    width: gameWidth,
+                    height: gameHeight,
+                    child: GameWidget.controlled(
+                      gameFactory: BrickBreaker.new,
+                      overlayBuilderMap: {
+                        PlayState.welcome.name: (context, game) => Center(
+                              child: Text(
+                                'TAP TO PLAY',
+                                style:
+                                    Theme.of(context).textTheme.headlineLarge,
+                              ),
+                            ),
+                        PlayState.gameOver.name: (context, game) => Center(
+                              child: Text(
+                                'G A M E   O V E R',
+                                style:
+                                    Theme.of(context).textTheme.headlineLarge,
+                              ),
+                            ),
+                        PlayState.gameWon.name: (context, game) => Center(
+                              child: Text(
+                                'Y O U   W O N ! ! !',
+                                style:
+                                    Theme.of(context).textTheme.headlineLarge,
+                              ),
+                            ),
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
