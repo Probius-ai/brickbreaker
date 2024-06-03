dc0cae958ece2f0c17a1493dcccb64a5dd805c42
Probius-ai <kcfm0529@naver.com>
2024-06-03 23:40:28 +0900

    Add Sound

--- lib/src/objects/brick.dart
+++ lib/src/objects/brick.dart
@@ -28,11 +28,7 @@ class Brick extends RectangleComponent
     // Implement the collision logic for the brick
     super.onCollisionStart(intersectionPoints, other);
     removeFromParent();
-    if (streak == 0) {
-      game.score.value += 1;
-    } else {
-      game.score.value += streak;
-    }
+    game.score.value += 1;
 
     if(game.world.children.query<Brick>().length == 1){
       game.playState = PlayState.gameWon;
