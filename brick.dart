8cbaed5e70da8d8fe924de4d6ff3eb076130571b
BTC100M <164449301+BTC100M@users.noreply.github.com>
2024-06-05 00:20:00 +0900

    Add files via upload

---
+++ brick.dart
@@ -0,0 +1,39 @@
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
+
+import '../brickbreakergame.dart';
+import '../config.dart';
+import 'ball.dart';
+import 'bat.dart';
+
+class Brick extends RectangleComponent 
+  with HasGameRef<BrickBreaker>, CollisionCallbacks{
+  // Add your custom properties and methods here
+  Brick({
+    required super.position,
+    required Color color,
+  }) : super(
+    size: Vector2(brickWidth, brickHeight),
+    anchor: Anchor.center,
+    paint: Paint()
+      ..color = color
+      ..style = PaintingStyle.fill,
+    children: [RectangleHitbox()],
+    );
+
+  @override
+  void onCollisionStart(
+    Set<Vector2> intersectionPoints, PositionComponent other) {
+    // Implement the collision logic for the brick
+    super.onCollisionStart(intersectionPoints, other);
+    removeFromParent();
+    game.score.value += 1;
+
+    if(game.world.children.query<Brick>().length == 1){
+      game.playState = PlayState.gameWon;
+      game.world.removeAll(game.world.children.query<Ball>());
+      game.world.removeAll(game.world.children.query<Bat>());
+    }
+  }
+}
---
+++ obj.dart
@@ -0,0 +1,5 @@
+export 'game_area.dart';
+export 'bat.dart';
+export 'ball.dart';
+export 'brick.dart';
+// Add more export statements for additional files
