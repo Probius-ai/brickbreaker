f7a51954561140a8e0ef92736a0821752197c11f
BTC100M <mongja1209@naver.com>
2024-06-07 10:14:40 +0900

    변경사항 : ball 객체를 만들어 브릭에 닿을시 공이 추가된다.

--- lib/src/objects/ball.dart
+++ lib/src/objects/ball.dart
@@ -9,45 +9,42 @@ import 'brick.dart';
 import 'game_area.dart';
 
 class Ball extends CircleComponent 
-  with HasGameReference<BrickBreaker>, CollisionCallbacks{
-  // Add your properties and methods here
+  with HasGameReference<BrickBreaker>, CollisionCallbacks {
+  final Vector2 velocity;
+  final double difficultyModifier;
+
   Ball({
     required this.velocity,
     required super.position,
     required double radius,
     required this.difficultyModifier,
-  }) :super(
+  }) : super(
     anchor: Anchor.center,
     radius: radius,
     paint: Paint()
-    ..color = Color.fromARGB(255, 255, 0, 0)
-    ..style = PaintingStyle.fill,
-    children: [CircleHitbox()],);
+      ..color = Color.fromARGB(255, 255, 0, 0)
+      ..style = PaintingStyle.fill,
+    children: [CircleHitbox()],
+  );
 
-
-  final Vector2 velocity;
-  final double difficultyModifier;
-  
   @override
   void update(double dt) {
-    // Add your update logic here
     super.update(dt);
     position += velocity * dt;
   }
 
   @override
-  void onCollisionStart(
-    Set<Vector2> intersectionPoints, PositionComponent other) {
-    // Add your collision logic here
+  void onCollisionStart(Set<Vector2> intersectionPoints, PositionComponent other) {
     super.onCollisionStart(intersectionPoints, other);
-    if(other is GameArea){// Check if the ball hits the game area
-      if (intersectionPoints.first.y <= 0){
+    
+    if (other is GameArea) {
+      if (intersectionPoints.first.y <= 0) {
         velocity.y *= -1;
-      } else if (intersectionPoints.first.x <= 0){
+      } else if (intersectionPoints.first.x <= 0) {
         velocity.x *= -1;
-      } else if (intersectionPoints.first.x >= game.width){
+      } else if (intersectionPoints.first.x >= game.width) {
         velocity.x *= -1;
-      } else if (intersectionPoints.first.y >= game.height){
+      } else if (intersectionPoints.first.y >= game.height) {
         add(RemoveEffect(
           delay: 0.35,
           onComplete: () {
@@ -55,22 +52,29 @@ class Ball extends CircleComponent
           },
         ));
       }
-    } else if (other is Bat){// Check if the ball hits the bat
+    } else if (other is Bat) {
       velocity.y *= -1;
-      velocity.x = velocity.x +
-        (position.x - other.position.x) / other.size.x * game.width * 0.3;
+      velocity.x = velocity.x + (position.x - other.position.x) / other.size.x * game.width * 0.3;
+    } else if (other is Brick) {
+      if (position.y < other.position.y - other.size.y / 2) {
+        velocity.y *= -1;
+      } else if (position.y > other.position.y + other.size.y / 2) {
+        velocity.y *= -1;
+      } else if (position.x < other.position.x) {
+        velocity.x *= -1;
+      } else if (position.x > other.position.x) {
+        velocity.x *= -1;
+      }
+      velocity.setFrom(velocity * difficultyModifier);
 
-    }else if (other is Brick){// Check if the ball hits a brick
-        if ( position.y < other.position.y - other.size.y / 2 ){
-          velocity.y *= -1;
-        } else if (position.y > other.position.y + other.size.y / 2){
-          velocity.y *= -1;
-        } else if (position.x < other.position.x){
-          velocity.x *= -1;
-        } else if (position.x > other.position.x){
-          velocity.x *= -1;
-        }
-        velocity.setFrom(velocity * difficultyModifier);// Increase the speed of the ball with difficultyModifier
+      // Create and add a new ball
+      final newBall = Ball(
+        velocity: Vector2(velocity.x, -velocity.y),
+        position: Vector2(position.x, position.y),
+        radius: radius,
+        difficultyModifier: difficultyModifier,
+      );
+      game.world.add(newBall); // Ensure new ball is added to the world
     }
   }
 }
